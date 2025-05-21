8ce796a58d1f1c8db93f1167add5b152eac9639b
Anusha Syeda <anoshasyeda1999@gmail.com>
2025-05-21 13:37:28 +0500

    Update main.py

--- library_api/main.py
+++ library_api/main.py
@@ -31,8 +31,8 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["https://read-stack-eight.vercel.app", "http://localhost:3000"],
-    allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_credentials=False,
+    allow_methods=["*"],
     allow_headers=["*"],
     expose_headers=["*"],
 )
